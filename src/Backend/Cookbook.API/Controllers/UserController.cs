14cde7ffff5a8a39782cdc8c7c9102792bf2ac2f
Vinicius Marx <viniciusmarx01@gmail.com>
2025-06-14 19:42:26 -0300

    :sparkles: adding endpoint to register an user

---
+++ src/Backend/Cookbook.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Cookbook.Communication.Requests;
+using Cookbook.Communication.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookbook.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(RegisterUserResponse), StatusCodes.Status201Created)]
+    public IActionResult Register(RegisterUserRequest request)
+    {
+        return Created();
+    }
+}
---
+++ src/Shared/Cookbook.Communication/Requests/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Cookbook.Communication.Requests;
+
+public class RegisterUserRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
