512a2a4eeeb5466e57ffcccf0462501ddf9e86c0
Vinicius Marx <viniciusmarx01@gmail.com>
2025-07-13 21:51:41 -0300

    :sparkles: add test for error handling when registering a user with an empty name

--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -1,10 +1,14 @@
 ﻿using CommomTestUtilities.Requests;
 using Cookbook.Communication.Responses;
+using Cookbook.Exceptions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Shouldly;
+using System.Globalization;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using WebApi.Test.InlineData;
 
 namespace WebApi.Test.User.Register;
 
@@ -25,5 +29,29 @@ public class RegisterUserTest(CustomWebApplicationFactory factory) : IClassFixtu
 
         responseData.ShouldNotBeNull();
         responseData.Name.ShouldBe(request.Name);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_EmptyName(string culture)
+    {
+        var request = RegisterUserRequestBuilder.Build();
+        request.Name = string.Empty;
+
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Clear();
+        _httpClient.DefaultRequestHeaders.AcceptLanguage.Add(new StringWithQualityHeaderValue(culture));
+
+        var response = await _httpClient.PostAsJsonAsync("User", request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+
+        var responseData = await response.Content.ReadFromJsonAsync<JsonDocument>();
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray().Select(e => e.GetString()).ToList();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("NAME_EMPTY", new CultureInfo(culture));
+
+        errors.ShouldHaveSingleItem();
+        errors.ShouldContain(expectedMessage);
     }
 }
